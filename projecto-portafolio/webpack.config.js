0fc66f45918af6dcd4ad6af25315ae057e50bfc0
Enrique <octepc.97@gmail.com>
2022-08-03 18:48:19 -0500

    added babel

--- projecto-portafolio/webpack.config.js
+++ projecto-portafolio/webpack.config.js
@@ -6,7 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+
   resolve: {
     extensions: [".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 };
